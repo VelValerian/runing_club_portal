ba6def197c690dad72a4388b3324205b55a43143
Valerian <@VelValerian>
2024-05-29 01:05:13 +0300

    Add modal menu

---
+++ frontend/jscript/modal-menu.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const modalMenu = document.getElementById('modal-menu');
+    const closeButton = document.getElementById('close-button');
+
+    hamburgerMenu.addEventListener('click', () => {
+        modalMenu.classList.add('open');
+        hamburgerMenu.classList.add('hidden'); // Скрываем hamburger-menu
+    });
+
+    closeButton.addEventListener('click', () => {
+        modalMenu.classList.remove('open');
+        hamburgerMenu.classList.remove('hidden'); // Показываем hamburger-menu снова
+    });
+});
