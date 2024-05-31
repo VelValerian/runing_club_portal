37be58b3a02cc3b18c5ec54bf6bbb80e601d52c8
Valerian <@VelValerian>
2024-05-31 22:00:30 +0300

    Complete @media (min-width: 769px) and (max-width: 1024px) Ref prev @media Add id for header logo in html + js

--- frontend/jscript/modal-menu.js
+++ frontend/jscript/modal-menu.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.getElementById('hamburger-menu');
+    const headerLogo = document.getElementById('header-logo');
     const modalMenu = document.getElementById('modal-menu');
     const closeButton = document.getElementById('close-button');
 
     hamburgerMenu.addEventListener('click', () => {
         modalMenu.classList.add('open');
         hamburgerMenu.classList.add('hidden'); // Скрываем hamburger-menu
+        headerLogo.classList.add('hidden'); // Скрываем header-logo
     });
 
     closeButton.addEventListener('click', () => {
         modalMenu.classList.remove('open');
         hamburgerMenu.classList.remove('hidden'); // Показываем hamburger-menu снова
+        headerLogo.classList.remove('hidden'); // Показываем header-logo снова
     });
+
 });
