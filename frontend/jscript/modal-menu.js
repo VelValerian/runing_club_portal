4226e8132c874365d7b5ec31230e9964bc09e092
Valerian <@VelValerian>
2024-06-01 03:20:07 +0300

    Complete @media (min-width: 1441px)

--- frontend/jscript/modal-menu.js
+++ frontend/jscript/modal-menu.js
@@ -7,13 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
     hamburgerMenu.addEventListener('click', () => {
         modalMenu.classList.add('open');
         hamburgerMenu.classList.add('hidden'); // Скрываем hamburger-menu
-        headerLogo.classList.add('hidden'); // Скрываем header-logo
     });
 
     closeButton.addEventListener('click', () => {
         modalMenu.classList.remove('open');
         hamburgerMenu.classList.remove('hidden'); // Показываем hamburger-menu снова
-        headerLogo.classList.remove('hidden'); // Показываем header-logo снова
     });
 
 });
