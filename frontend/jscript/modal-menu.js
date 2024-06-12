6884eb62fea810c9d251c6947d385d62de3aed68
Valerian <@VelValerian>
2024-06-12 23:35:04 +0300

    "page Blog" Complete @media (max-width: 640px) add script to modal-menu.js for scrolling modal-menu

--- frontend/jscript/modal-menu.js
+++ frontend/jscript/modal-menu.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.getElementById('hamburger-menu');
-    const headerLogo = document.getElementById('header-logo');
     const modalMenu = document.getElementById('modal-menu');
     const closeButton = document.getElementById('close-button');
 
     hamburgerMenu.addEventListener('click', () => {
         modalMenu.classList.add('open');
         hamburgerMenu.classList.add('hidden'); // Скрываем hamburger-menu
+        document.body.style.overflow = 'hidden'; // Запрещаем прокрутку
     });
 
     closeButton.addEventListener('click', () => {
         modalMenu.classList.remove('open');
         hamburgerMenu.classList.remove('hidden'); // Показываем hamburger-menu снова
+        document.body.style.overflow = 'auto'; // Восстанавливаем прокрутку
     });
 
 });
