bef3bbac1ad7158e5adee4059afee76c7b863ab9
Valerian <@VelValerian>
2024-06-24 07:04:35 +0300

    "Header" refactor header menu for all page to desktop screen

--- frontend/jscript/modal-menu.js
+++ frontend/jscript/modal-menu.js
@@ -2,23 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const modalMenu = document.getElementById('modal-menu');
     const closeButton = document.getElementById('close-button');
-    const logoModal = document.getElementById('logo-modal');
-    const footerModal = document.getElementById('footer-modal');
 
     hamburgerMenu.addEventListener('click', () => {
         modalMenu.classList.add('open');
         hamburgerMenu.classList.add('hidden'); // Скрываем hamburger-menu
-        closeButton.style.display = 'initial';
-        logoModal.style.display = 'flex';
-        footerModal.style.display = 'flex';
     });
 
     closeButton.addEventListener('click', () => {
         modalMenu.classList.remove('open');
         hamburgerMenu.classList.remove('hidden'); // Показываем hamburger-menu снова
-        closeButton.style.display = 'none';
-        logoModal.style.display = 'none';
-        footerModal.style.display = 'none';
     });
 
 });
