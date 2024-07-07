755ab224090066b924d9cd63750d9ea9c61204d4
Valerian <@VelValerian>
2024-07-07 13:29:06 +0300

    Refactor header change modal menu on all media resolutions

--- frontend/jscript/modal-menu.js
+++ frontend/jscript/modal-menu.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.getElementById('hamburger-menu');
-    const modalMenu = document.getElementById('modal-menu');
+    const hamburgerHead = document.getElementById('hamburger-head');
+    const modalHead = document.getElementById('modal-head');
     const closeButton = document.getElementById('close-button');
 
     hamburgerMenu.addEventListener('click', () => {
-        modalMenu.classList.add('open');
-        hamburgerMenu.classList.add('hidden'); // Скрываем hamburger-menu
+        modalHead.classList.add('open');
+        hamburgerHead.classList.add('hidden'); // Скрываем hamburger-menu
     });
 
     closeButton.addEventListener('click', () => {
-        modalMenu.classList.remove('open');
-        hamburgerMenu.classList.remove('hidden'); // Показываем hamburger-menu снова
+        modalHead.classList.remove('open');
+        hamburgerHead.classList.remove('hidden'); // Показываем hamburger-menu снова
     });
 
 });
